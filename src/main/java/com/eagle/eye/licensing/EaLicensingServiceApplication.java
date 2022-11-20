6ff3ae67471967d40d821acafb574ea7e84e8e6b
CyberAlexander <taurum13@gmail.com>
2022-11-20 11:20:53 +0300

    Added initial structure for the Licensing Micro-Service, which is the part of Eagle Eye Application.

---
+++ src/main/java/com/eagle/eye/licensing/EaLicensingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.eagle.eye.licensing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaLicensingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EaLicensingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eagle/eye/licensing/EaLicensingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eagle.eye.licensing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EaLicensingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
