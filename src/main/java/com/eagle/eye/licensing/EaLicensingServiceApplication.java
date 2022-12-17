07ae81a3cb0395ef9d67a3d1657069de3f72ea82
CyberAlexander <taurum13@gmail.com>
2022-12-17 12:19:12 +0300

    Working on EA Licensing Service implementation

--- src/main/java/com/eagle/eye/licensing/EaLicensingServiceApplication.java
+++ src/main/java/com/eagle/eye/licensing/EaLicensingServiceApplication.java
@@ -2,15 +2,31 @@ package com.eagle.eye.licensing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.UUID;
-
+/**
+ * The {@link EnableDiscoveryClient} annotation is the trigger for Spring Cloud to enable the application to use the
+ * DiscoveryClient and Ribbon libraries.
+ * <p>
+ * Note:
+ * >>> Should be used {@link EnableDiscoveryClient} or {@link EnableFeignClients}
+ */
 @SpringBootApplication
+@EnableDiscoveryClient /* Activates the Spring DiscoveryClient for use */
+/*@EnableFeignClients*/ /* Needed to use the FeignClient in your code */
 public class EaLicensingServiceApplication {
 
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
     public static void main(String[] args) {
-        System.out.println(UUID.randomUUID());
-        System.out.println(UUID.randomUUID());
         SpringApplication.run(EaLicensingServiceApplication.class, args);
     }
 
