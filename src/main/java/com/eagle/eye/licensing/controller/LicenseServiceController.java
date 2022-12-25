df501d4a7fac47ff05b7241c32dd3823435739c4
CyberAlexander <taurum13@gmail.com>
2022-12-25 14:25:22 +0300

    Exposed new end points in LicenseController

--- src/main/java/com/eagle/eye/licensing/controller/LicenseServiceController.java
+++ src/main/java/com/eagle/eye/licensing/controller/LicenseServiceController.java
@@ -4,9 +4,17 @@ import com.eagle.eye.licensing.config.ServiceConfig;
 import com.eagle.eye.licensing.model.License;
 import com.eagle.eye.licensing.service.LicenseService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,9 +28,10 @@ import java.util.UUID;
  * @author Aliaksandr_Leanovich
  * @version 1.0
  */
+@Slf4j
 @RestController
 @AllArgsConstructor
-@RequestMapping(value = "/v1/organisations/{organisationId}/licenses")
+@RequestMapping(path = "/v1/organisations/{organisationId}/licenses", produces = MediaType.APPLICATION_JSON_VALUE)
 public class LicenseServiceController {
 
     private LicenseService licenseService;
@@ -45,5 +54,24 @@ public class LicenseServiceController {
             @PathVariable UUID licenseId,
             @PathVariable String clientType) {
         return licenseService.getLicense(organisationId, licenseId, clientType);
+    }
+
+    @PutMapping(value = "/{licenseId}")
+    public void updateLicense(@PathVariable("licenseId") UUID licenseId, @RequestBody License license) {
+        log.debug("Updating license:{}", licenseId);
+        licenseService.saveLicense(license);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void saveLicense(@RequestBody License license) {
+        licenseService.saveLicense(license);
+    }
+
+    @DeleteMapping(value = "/{licenseId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteLicense(@PathVariable("licenseId") UUID licenseId, @RequestBody License license) {
+        log.debug("Deleting License:{}", licenseId);
+        licenseService.deleteLicense(license);
     }
 }
