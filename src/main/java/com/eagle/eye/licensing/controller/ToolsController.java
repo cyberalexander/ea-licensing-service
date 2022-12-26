2b14a55cdc624f72cf86e813eabbbf975f2b2c61
CyberAlexander <taurum13@gmail.com>
2022-12-26 10:12:25 +0300

    Exposed an end point to get the list of services exposed and registered in Eureka

---
+++ src/main/java/com/eagle/eye/licensing/controller/ToolsController.java
@@ -0,0 +1,54 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022 cyberalexander
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.eagle.eye.licensing.controller;
+
+import com.eagle.eye.licensing.service.DiscoveryService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Created : 26/12/2022 09:44
+ * Project : ea-licensing-service
+ * IDE : IntelliJ IDEA
+ *
+ * @author Aliaksandr_Leanovich
+ * @version 1.0
+ */
+@RestController
+@RequestMapping(path = "/v1/tools")
+@AllArgsConstructor
+public class ToolsController {
+
+    private DiscoveryService discoveryService;
+
+    @GetMapping(path = "/eureka/services")
+    public List<String> getEurekaServices() {
+        return discoveryService.getEurekaServices();
+    }
+}
