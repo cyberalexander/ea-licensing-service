10acebc993aec795c8e7a611deac5874662172f8
CyberAlexander <taurum13@gmail.com>
2023-04-06 09:27:30 +0300

    Implementing unit tests for ea-licensing-service business logic

--- src/test/java/com/eagle/eye/licensing/EaLicensingServiceApplicationTests.java
+++ src/test/java/com/eagle/eye/licensing/EaLicensingServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.eagle.eye.licensing;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles({"test"})
 class EaLicensingServiceApplicationTests {
 
     @Test
